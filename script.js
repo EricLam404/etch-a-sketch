053d4e217799cc7525d082a43f253c7b9b0e7540
EricLam404 <lameric9888@gmail.com>
2022-07-16 09:13:58 -0400

    added the 16x16 grid

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-console.log("works");
+let container = document.createElement('div');
+container.classList.add('container')
+
+for(let col = 0; col < 16; col++){
+    let r = document.createElement('div');
+    r.classList.add('row')
+    for(let row = 0; row < 16; row++){
+        let square = document.createElement('div');
+        square.classList.add('square');
+        r.appendChild(square);
+    }
+    container.appendChild(r);
+}
+
+document.body.appendChild(container)
+
+
