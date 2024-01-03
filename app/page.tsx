62f8b6acdb38eb56a55a62d30504e1de4116b500
EricLam404 <lameric9888@gmail.com>
2024-01-02 19:16:02 -0500

    Made canvas draggable

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,17 @@
+'use client'
+
+import Draggable from 'react-draggable';
 import Canvas from "./components/canvas"
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center p-5">
-      <h1>Etch a sketch</h1>
-      <Canvas />
+        <h1>Etch a sketch</h1>
+        <Draggable>
+            <div>
+                <Canvas/>
+            </div>
+        </Draggable>
     </main>
   )
 }
